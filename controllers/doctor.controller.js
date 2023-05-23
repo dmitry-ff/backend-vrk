37775c3ddf7a542965a9a5121d969e2c1ddcee0f
Dmitry Kozlov <dima.kozlov.2186@gmail.com>
2023-05-23 18:38:35 +0300

    add files for controllers add cotrollers for creation doctor and card entities

---
+++ controllers/doctor.controller.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+const Doctor = db.Doctor;
+
+exports.addDoctor = function(req, res) {
+  if(!req.body) {
+    res.status(400).send({
+      message: "Contetn can not be empty!",
+    });
+    return;
+  }
+  Doctor.create(req.body)
+    .then((data) => {
+      res.send(data)
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occured while create the Doctor"
+      })
+    })
+}
